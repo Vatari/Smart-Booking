e01a87bef5b503db5b4e365bf7118b34c47433f3
vatari <majestic13@gmail.com>
2022-09-29 10:23:00 +0300

    added config files

---
+++ config/dbConfig.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+require("../models/User");
+
+const dbName = "booking-uni";
+const connectionString = `mongodb://localhost:27017/${dbName}`;
+
+module.exports = async (app) => {
+  try {
+    await mongoose.connect(connectionString, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+
+    console.log("Database connection established");
+    mongoose.connection.on("error", (err) => {
+      console.log("Database connection error");
+      console.log(err);
+    });
+  } catch (err) {
+    console.error("Error connecting to database");
+    process.exit(1);
+  }
+};
+
